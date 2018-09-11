963821304e9e439949786ede20546bab25108206
甘泉 <960300358@qq.com>
2018-09-11 18:07:38 +0800

    版本3.2提交

--- app/src/main/java/com/shuyun/qapp/ui/login/RegisterPhoneActivity.java
+++ app/src/main/java/com/shuyun/qapp/ui/login/RegisterPhoneActivity.java
@@ -1,5 +1,6 @@
 package com.shuyun.qapp.ui.login;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -43,11 +44,12 @@ public class RegisterPhoneActivity extends BaseActivity implements View.OnClickL
         ButterKnife.bind(this);
         ivBack.setOnClickListener(this);
         llAgreeText.setOnClickListener(this);
+        ivClearPhoneNum.setOnClickListener(this);
 
         if ("login".equals(getIntent().getStringExtra("name"))) {
             tvTitle.setText("新用户注册");
         }
-
+        clearEditText(etPhoneNumber, ivClearPhoneNum);
         etPhoneNumber.addTextChangedListener(new TextWatcher() {//没有输入正确格式的手机号,登录按钮置灰
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -61,13 +63,7 @@ public class RegisterPhoneActivity extends BaseActivity implements View.OnClickL
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (!EncodeAndStringTool.isStringEmpty(etPhoneNumber.getText().toString())) {
-                    btnNext.setEnabled(true);
-                    btnNext.setTextColor(Color.parseColor("#ffffff"));
-                } else {
-                    btnNext.setEnabled(false);
-                    btnNext.setTextColor(Color.parseColor("#999999"));
-                }
+                isLogin();
             }
         });
     }
@@ -83,11 +79,70 @@ public class RegisterPhoneActivity extends BaseActivity implements View.OnClickL
             case R.id.iv_back:
                 finish();
                 break;
+            case R.id.iv_clear_phone_num:
+                etPhoneNumber.setText("");
+                ivClearPhoneNum.setVisibility(View.GONE);
+                isLogin();
+                break;
             case R.id.ll_agree_text:
-
+                //跳转到协议界面
+                startActivity(new Intent(this, UserAgreementActivity.class));
                 break;
             default:
                 break;
+        }
+    }
+
+    /**
+     * 清空编辑框
+     *
+     * @param editText 需要清空的EditText控件
+     * @param clearPic 清空数据的图片
+     */
+    protected void clearEditText(final EditText editText, final ImageView clearPic) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (editText.getText().length() != 0) {
+                    clearPic.setVisibility(View.VISIBLE);
+                } else {
+                    clearPic.setVisibility(View.GONE);
+                }
+            }
+        });
+        /**
+         * 焦点变化监听
+         */
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (editText.getText().length() != 0 && hasFocus) {//不等于0,切得到焦点
+                    clearPic.setVisibility(View.VISIBLE);
+                    isLogin();
+                } else {
+                    clearPic.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void isLogin() {
+        if (!EncodeAndStringTool.isStringEmpty(etPhoneNumber.getText().toString())) {
+            btnNext.setEnabled(true);
+            btnNext.setTextColor(Color.parseColor("#ffffff"));
+        } else {
+            btnNext.setEnabled(false);
+            btnNext.setTextColor(Color.parseColor("#999999"));
         }
     }
 }
