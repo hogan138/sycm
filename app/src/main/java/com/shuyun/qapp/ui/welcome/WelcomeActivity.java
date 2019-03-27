a78001508c0bc9410e0102701cbb912f888fd86d
甘泉 <960300358@qq.com>
2019-03-27 15:11:28 +0800

    4.0.0版本

--- app/src/main/java/com/shuyun/qapp/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/shuyun/qapp/ui/welcome/WelcomeActivity.java
@@ -84,13 +84,13 @@ public class WelcomeActivity extends BaseActivity implements OnRemotingCallBackL
         ButterKnife.bind(this);
         mContext = this;
 
-        DisplayMetrics dm = this.getResources().getDisplayMetrics();
-        int w = (int) Math.ceil(dm.widthPixels);
-        height = (int) Math.ceil(w * (722f / 504f));
-
-        ViewGroup.LayoutParams params = mainImg.getLayoutParams();
-        params.height = height;
-        mainImg.setLayoutParams(params);
+//        DisplayMetrics dm = this.getResources().getDisplayMetrics();
+//        int w = (int) Math.ceil(dm.widthPixels);
+//        height = (int) Math.ceil(w * (722f / 504f));
+//
+//        ViewGroup.LayoutParams params = mainImg.getLayoutParams();
+//        params.height = height;
+//        mainImg.setLayoutParams(params);
 
         //腾讯应用分析
         StatConfig.init(this);
